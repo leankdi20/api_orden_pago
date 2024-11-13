f4be805726bd7f05ccb0940569aec73195b4542a
Varela Leandro <98416431+leankdi20@users.noreply.github.com>
2024-11-13 10:01:09 -0600

    agregado el corse para los headers

--- app/api_orden_pago/settings.py
+++ app/api_orden_pago/settings.py
@@ -32,10 +32,13 @@ ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
 if(ALLOWED_HOSTS_ENV):
     ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
